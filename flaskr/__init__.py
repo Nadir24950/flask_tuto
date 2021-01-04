5e612a83772d3e32e9e6f03be67672e125cc6b71
Nadir Belmokhtar <ndir24950@gmail.com>
2021-01-04 19:36:09 +0100

    add db.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,10 @@
 import os
-from os.path import expanduser
+
 from flask import Flask
 
+
 def create_app(test_config=None):
-    #create and configue the app
+    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -11,23 +12,20 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        #load the instance config, if it exists, when not testing
+        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
-
     else:
-        #load the test config if passed in
+        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    
-    #ensure the instance folder exists 
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    #a simple page of hello world 
-    @app.route('/hello')
-    
+    # a simple page that says hello
+    @app.route('/')
     def hello():
         return 'Hello, World!'
 
